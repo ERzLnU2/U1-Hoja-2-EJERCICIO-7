470ca6482b168e036709b970b431f6eb118d9e85
solomongo™ <80227002+solomongoGUI@users.noreply.github.com>
2022-09-26 01:19:02 +0200

    Create main.java

---
+++ main.java
@@ -0,0 +1,19 @@
+/**
+ * @author DAW120
+   📌 EJERCICIO 7, Hoja 2.   
+  
+   🔴 Programa que asigna los valores sin pedirlos por pantalla a una variable 
+      a el valor 23 y a otra variable b el valor 56. 
+      
+   🔴 Intercambiar el valor de dichas variables y mostrar por pantalla el valor 
+     que tenían antes y después del intercambio.
+🚀 🏴‍
+ */
+
+package com.solomongo.ejercicio7_hoja2;
+public class Ejercicio7_Hoja2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
